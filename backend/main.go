6340cc15e119887891d32bcfdfe4b560248c5895
Guilherme Pereira <guilospanck@protonmail.com>
2024-06-21 19:35:14 +0100

    Add websocket

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,30 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/net/websocket"
 )
+
+// To test via CLI: wscat -H "Origin: http://localhost:4444" -c ws://localhost:4444/ws
+func ws(c echo.Context) error {
+	response := `
+	<div>
+	<p style="background: red; width: 10px; height: 10px"></p>
+	<p style="background: black; width: 10px; height: 10px"></p>
+	<p style="background: red; width: 10px; height: 10px"></p>
+	</div>
+	`
+	websocket.Handler(func(ws_connection *websocket.Conn) {
+		defer ws_connection.Close()
+		for {
+			// Write
+			err := websocket.Message.Send(ws_connection, response)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
+}
 
 func main() {
 	e := echo.New()
@@ -17,9 +40,10 @@ func main() {
 
 	e.GET("/ping", func(c echo.Context) error {
 		response := "<p>I AM TESTING</p>"
-		// c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		return c.String(http.StatusOK, response)
 	})
+
+	e.GET("/ws", ws)
 
 	e.Logger.Fatal(e.Start(":4444"))
 }
