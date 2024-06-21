56c60126199814a3220bba5451cbfd465487e488
Guilherme Pereira <guilospanck@protonmail.com>
2024-06-21 18:04:50 +0100

    First working html - htmx - go

--- backend/main.go
+++ backend/main.go
@@ -4,13 +4,21 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
 
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
 	e.GET("/ping", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Pong!")
+		response := "<p>I AM TESTING</p>"
+		// c.Response().Header().Set("Access-Control-Allow-Origin", "*")
+		return c.String(http.StatusOK, response)
 	})
 
 	e.Logger.Fatal(e.Start(":4444"))
